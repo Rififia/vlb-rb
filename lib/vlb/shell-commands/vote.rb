c4f9965d491462c9e3bc82f1e46bb90bd0eae3fe
Personne Secret <Rififia@users.noreply.github.com>
2015-10-12 18:42:14 +0200

    Update vote.rb

--- lib/vlb/shell-commands/vote.rb
+++ lib/vlb/shell-commands/vote.rb
@@ -50,10 +50,8 @@ module VikiLinkBot
         case line.downcase
             when /\A(?>[#*]\s*)\{\{?\b/
               votes[$1]
-            end
             when /\A(?>[#*]\s*)\w/
               votes[section]
-            end
         end
         if votes.values.reduce(0, &:+) == 0
             "Personne n'a encore voté, ou bien je ne sais pas reconnaitre les votes."
